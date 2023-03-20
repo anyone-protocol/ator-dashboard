3d6130aae5ba175612f24d0c5d0c1da64f20bc5b
Jim Toth <jim@artby.city>
2023-03-20 19:54:11 -0400

    WIP: adds relay registration form

--- composables/signer.ts
+++ composables/signer.ts
@@ -1,6 +1,8 @@
 export const useSigner = async () => {
   const provider = useProvider()
   if (!provider) {
+    // TODO -> inform user they must connect with browser wallet
+
     return null
   } else {
     return await provider.getSigner()
--- pages/register.vue
+++ pages/register.vue
@@ -1,3 +1,44 @@
 <template>
-  <div class="register-page"></div>
+  <v-container class="register-page">
+    <v-form>
+      <v-text-field v-model="fingerprint" label="Relay Fingerprint" />
+      <v-btn @click="register" color="primary">Register</v-btn>
+    </v-form>
+  </v-container>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const fingerprint = ref('')
+const loading = ref(false)
+
+// TODO -> debounce
+const register = async () => {
+  const config = useRuntimeConfig()
+  const signer = await useSigner()
+
+  if (signer) {
+    loading.value = true
+
+    try {
+      const message = { method: 'register', fingerprint: fingerprint.value }
+      const signature = await signer.signMessage(JSON.stringify(message))
+
+      const res = await $fetch(`${config.public.api}/relays`, {
+        method: 'POST',
+        query: { address: signer.address, signature },
+        body: message
+      })
+
+      console.log('res', res)
+    } catch (error) {
+      console.error(error)
+      // TODO -> handle signing errors
+      // TODO -> handle api errors
+    }
+
+    loading.value = false
+  }
+}
+</script>
