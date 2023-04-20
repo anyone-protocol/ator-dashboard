32a41229fe0ce313c3f95cb88a28c6a2bf8c6fb8
jsb7131 <jasonstephenbaker@gmail.com>
2023-04-20 11:48:07 -0400

    Moved images / logo to public folder

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
         <router-link to="/" custom v-slot="{navigate}">
           <div class="logo-container" @click="navigate">
             <div class="top-bar-title">ATOR</div>
-            <img src="@/assets/images/AtorLogo.png" width="30" height="30" />
+            <img src="/images/AtorLogo.png" width="30" height="30" />
           </div>
         </router-link>
       </template>
