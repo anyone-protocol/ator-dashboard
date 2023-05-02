040ff1b89231bec7da627551a3cb547b9580c993
Jim Toth <jim@artby.city>
2023-05-02 18:47:15 -0400

    26 cicid deploy (#30)
    * Adds github actions cicd
    
    * updates cicd
    
    * hopefully fixes deploy!

--- deploy.mjs
+++ deploy.mjs
@@ -1,38 +1,48 @@
 import Bundlr from '@bundlr-network/client'
-import { WarpFactory, defaultCacheOptions } from 'warp-contracts'
+import { LoggerFactory, WarpFactory, defaultCacheOptions } from 'warp-contracts'
 import Arweave from 'arweave'
 
 const ANT = 'PspdulWYOoBov3dJyA9TdXuqDqaV7snSiwPjtesLvNU'
 const DEPLOY_FOLDER = './.output/public'
 const BUNDLR_NODE = 'https://node2.bundlr.network'
 
-const arweave = Arweave.init({
-  host: 'arweave.net',
-  port: 443,
-  protocol: 'https'
-})
-const jwk = JSON.parse(
-  Buffer.from(process.env.PERMAWEB_KEY, 'base64').toString('utf-8')
-)
+  const arweave = Arweave.init({
+    host: 'arweave.net',
+    port: 443,
+    protocol: 'https'
+  })
+  const jwk = JSON.parse(
+    Buffer.from(
+      process.env.PERMAWEB_KEY || 'NO_KEY', 'base64'
+    ).toString('utf-8')
+  )
+  LoggerFactory.INST.logLevel('fatal')
+  const bundlr = new Bundlr(BUNDLR_NODE, 'arweave', jwk)
+  const warp = WarpFactory.custom(
+    arweave,
+    defaultCacheOptions,
+    'mainnet'
+  ).useArweaveGateway().build();
+  const contract = warp.contract(ANT).connect(jwk)
 
-const bundlr = new Bundlr(BUNDLR_NODE, 'arweave', jwk)
-const warp = WarpFactory.custom(
-  arweave,
-  defaultCacheOptions,
-  'mainnet'
-).useArweaveGateway().build()
-const contract = warp.contract(ANT).connect(jwk)
+  // upload folder
+  const result = await bundlr.uploadFolder(DEPLOY_FOLDER, {
+    indexFile: 'index.html'
+  })
 
-// upload folder
-const result = await bundlr.uploadFolder(DEPLOY_FOLDER, {
-  indexFile: 'index.html'
-})
+  // update ANT
+  if (result) {
+    console.log('bundlr result id', result.id)
+    const deployed = await contract.writeInteraction({
+      function: 'setRecord',
+      subDomain: '@',
+      transactionId: result.id
+    })
 
-// update ANT
-await contract.writeInteraction({
-  function: 'setRecord',
-  subDomain: '@',
-  transactionId: result.id
-})
-
-console.log('Deployed!  Please wait 20 - 30 minutes for ArNS to update!')
+    console.log(
+      'Deployed!  Please wait 20 - 30 minutes for ArNS to update!',
+      deployed
+    )
+  } else {
+    console.error('Bundlr result error', result)
+  }
