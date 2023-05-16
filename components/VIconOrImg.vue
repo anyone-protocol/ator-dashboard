89acb950829b6d09e7bf267b9a5e999a6f0ecb6a
jsb7131 <jasonstephenbaker@gmail.com>
2023-05-16 01:00:10 -0400

    swapped eth logo for ator in nav list items

---
+++ components/VIconOrImg.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-icon v-if="icon" class="list-item-icon" :icon="icon"></v-icon>
+  <img v-else-if="pathToImg" class="black-and-white" :src="pathToImg" width="24" height="24" />
+  <!-- icon is prioritized if both props are passed in and have values -->
+  <div v-else style="width:24px;height:24px"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+    pathToImg: String
+  }
+};
+</script>
