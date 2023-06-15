33469d8a63c7deb9a7dc40917426bcd20385b486
jsb7131 <jasonstephenbaker@gmail.com>
2023-06-15 18:49:20 -0400

    relay registry address displays in pop up menu

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer app border>
     <v-row>
-      <v-col cols="3">
+      <v-col cols="4">
         <a
           class="basic-text"
           target="_blank"
@@ -18,16 +18,32 @@
         <v-btn color="primary" variant="plain" size="small">
           {{appTheme.global.name.value}}
           <v-menu activator="parent" offset-y>
-          <v-list>
-            <v-list-item class="theme-menu-list-item" v-for="theme in themes" :key="theme" @click="changeTheme(theme)">
-              <div class="theme-menu-list-item-container"><v-list-item-title>{{ theme }}</v-list-item-title></div>
-            </v-list-item>
-          </v-list>
-        </v-menu>
+            <v-list>
+              <v-list-item class="theme-menu-list-item" v-for="theme in themes" :key="theme" @click="changeTheme(theme)">
+                <div class="theme-menu-list-item-container"><v-list-item-title>{{ theme }}</v-list-item-title></div>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </v-btn>
       </v-col>
-      <v-col cols="6" class="">
-        Relay Registry:
+      <v-col cols="5" style="text-align:end">
+        <v-btn color="primary" variant="plain" size="small">
+          Relay Registry
+          <v-menu activator="parent" offset-y>
+            <v-list>
+              <v-list-item class="theme-menu-list-item" @click="goToRelayRegSonar()">
+                <div class="theme-menu-list-item-container">
+                  <v-list-item-title>
+                    <code class="basic-text">
+                      {{ runtimeCfg.public.relayRegistryAddress }}
+                    </code>
+                  </v-list-item-title>
+                </div>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-btn>
+        <!-- Relay Registry:
         <code>          
           <a
             class="basic-text"
@@ -36,7 +52,7 @@
           >
             {{ runtimeCfg.public.relayRegistryAddress }}
           </a>
-        </code>
+        </code> -->
       </v-col>
     </v-row>
   </v-footer>
@@ -59,5 +75,9 @@ const changeTheme = (theme: string) => {
   if (localStorage) {
     localStorage.setItem('theme', theme)
   }
+}
+
+const goToRelayRegSonar = () => {
+  window.open(relayRegistrySonarUrl, '_blank')?.focus()
 }
 </script>
