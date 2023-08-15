fe6b7ee10ac84e6f791bc2fa687a1f4261c05a15
Jim Toth <jim@artby.city>
2023-08-14 22:39:22 -0400

    Cleanup signer

--- composables/signer.ts
+++ composables/signer.ts
@@ -4,15 +4,12 @@ export const useSigner = async () => {
   let provider = useProvider()
 
   if (provider instanceof AbstractProvider) {
-    console.log('provider is AbtractProvider')
     provider = initializeBrowserProvider()
   }
 
   if (provider instanceof BrowserProvider) {
-    console.log('provider is BrowserProvider')
     try {
       const signer = await (provider as BrowserProvider).getSigner()
-      console.log('provider._network.name',provider._network.name)
       if (provider._network.name !== NETWORKS.GOERLI.name) {
         await window.ethereum!.request({
           method: 'wallet_switchEthereumChain',
@@ -27,8 +24,6 @@ export const useSigner = async () => {
       console.error('Error getting Signer', error)
     }
   }
-
-  console.log('return null')
 
   return null
 }
